8077c57d5b9206ad80e723d02feccfea60b317ab
siva venkat gogineni <gogineni.sivavenkat@gmail.com>
2017-03-02 16:53:25 -0500

    Removing CSV and adding TSV and removing sops

--- src/com/distil/SimpleThreadPool.java
+++ src/com/distil/SimpleThreadPool.java
@@ -26,7 +26,7 @@ class CallableClass implements Callable<Result> {
         this.rResult= new Result(sFilename, new ArrayList<String>());
     }
     public Result call() throws Exception {
-        System.out.println("Reading file:"+sFilename);
+        //System.out.println("Reading file:"+sFilename);
         List<String> listOfLines= new ArrayList<String>();
         InputStream in = Files.newInputStream(Paths.get(sFilename));
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
@@ -46,32 +46,32 @@ class CallableClass implements Callable<Result> {
 class SimpleThreadPool {
     /**
      * This method performs the necessary Aggregations as requested in the document per domain and displays it in the console.
-     * @param listCsvLines: List of strings, where in each string represents a line read from the CSV file.
+     * @param listCsvLines: List of strings, where in each string represents a line read from the tsv file.
      */
    public static void doTheAggregates(List<String> listCsvLines)
    {
        //System.out.println("Finished Reading All files Concurrently using  threads!!! Hurray!!!");
        //System.out.println("Total Requests across all domains::" + listCsvLines.size());
-       Map<String, List<Csv>> mapDomainToListCsvs = new HashMap<String, List<Csv>>();
+       Map<String, List<Tsv>> mapDomainToListCsvs = new HashMap<String, List<Tsv>>();
        List<DomainRecord> listOfDomainRecords = new ArrayList<DomainRecord>();
 
-       // Mapping the csv lines into a map, where the key is the domain name and the value is a list of Csv objects. Each of the csv objects represents a csv line.
+       // Mapping the tsv lines into a map, where the key is the domain name and the value is a list of Tsv objects. Each of the tsv objects represents a tsv line.
        for (String resultLine : listCsvLines) {
            String[] columns = resultLine.split("\t");
            String sDomain=columns[1];
            Date dRequestDateObject = new Date(Double.valueOf((Double.parseDouble(columns[0]) * 1000)).longValue());
-           Csv csvRecord=new Csv(dRequestDateObject,sDomain);
-           List<Csv> listOfCsvsForADomain= new ArrayList<Csv>();
+           Tsv tsvRecord =new Tsv(dRequestDateObject,sDomain);
+           List<Tsv> listOfCsvsForADomain= new ArrayList<Tsv>();
            if(mapDomainToListCsvs.containsKey(sDomain))
                listOfCsvsForADomain=mapDomainToListCsvs.get(sDomain);
-           listOfCsvsForADomain.add(csvRecord);
+           listOfCsvsForADomain.add(tsvRecord);
            mapDomainToListCsvs.put(sDomain, listOfCsvsForADomain);
        }
        // Converting the above map into a list of DomainRecord objects where in each object represents a domain.
-       for(Map.Entry<String, List<Csv>> resultLinesPerDomainEntry : mapDomainToListCsvs.entrySet()){
+       for(Map.Entry<String, List<Tsv>> resultLinesPerDomainEntry : mapDomainToListCsvs.entrySet()){
            DomainRecord domainRecord=  new DomainRecord(resultLinesPerDomainEntry.getKey());
-           for(Csv csv:resultLinesPerDomainEntry.getValue()) {
-               domainRecord.addRequestTime(csv.getRequestTime());
+           for(Tsv tsv :resultLinesPerDomainEntry.getValue()) {
+               domainRecord.addRequestTime(tsv.getRequestTime());
            }
            listOfDomainRecords.add(domainRecord);
        }
@@ -98,7 +98,7 @@ class SimpleThreadPool {
            System.out.println("Maximum requests per hour for this domain :" + (Collections.max(mapOfDateHourKeyToNoOfRequests.values())));
            System.out.println();
        }
-      // System.out.println("Done. Thanks for the opportunity. I thoroughly Enjoyed It!!!");
+       System.out.println("Done. Thanks for the opportunity. I thoroughly Enjoyed It!!!");
    }
 
 
@@ -111,10 +111,12 @@ class SimpleThreadPool {
 
        String sFilePath= sUserDir+File.separator+"resources"+File.separator;
        File sPath = new File(sFilePath);
-       if (!sPath.isDirectory())
-           sFilePath= sUserDir.substring(0, sUserDir.length() - 4)+File.separator+"resources"+File.separator;
-       System.out.println("sUserDir::::::"+sUserDir);
-       System.out.println("sFilePath::::::"+sFilePath);
+       /**making sure that the directory exists else altering the path such that it works**/
+       if (!sPath.isDirectory()) {
+           sFilePath = sUserDir.substring(0, sUserDir.length() - 4) + File.separator + "resources" + File.separator;
+       }
+       //System.out.println("sUserDir::::::"+sUserDir);
+       //System.out.println("sFilePath::::::"+sFilePath);
        List<String> listOfFilePaths = new ArrayList<String>();
        File[] files = new File(sFilePath).listFiles();
        for (File file : files)
@@ -138,7 +140,10 @@ class SimpleThreadPool {
                        List<String> localListCsvLines = new ArrayList<String>();
 
                        List<Future<Result>> localSynchronizedListOfFutures=finalSynchronizedListOfFutures;
-                       System.out.println("Shutdown hook ran!");
+                       System.out.println();
+                       System.out.println(" Alert!!!!!!!!!!!!! Shutdown hook has been invoked!!!!!!!!!!");
+                       System.out.println();
+
                        //System.out.println("synchronizedListOfFutures"+localSynchronizedListOfFutures);
                        try {
                            while (localSynchronizedListOfFutures.size() > 0) {
